aeebb59dfcebdd261ddc934c68a5ec22454d75ce
barstoolbluz <steve@flox.dev>
2025-11-23 21:20:06 -0500

    ...
    Signed-off-by: barstoolbluz <steve@flox.dev>

--- .flox/pkgs/reinvent-demo-ticket-generator.nix
+++ .flox/pkgs/reinvent-demo-ticket-generator.nix
@@ -4,7 +4,7 @@
   python313Packages,
   stdenv,
 }: let
-  version = "0.1.0";
+  version = "0.1.1";
 
   # Python dependencies for the ticket generator
   pythonEnv = python313.withPackages (ps: with ps; [
--- .flox/pkgs/reinvent-demo-ticket-processor.nix
+++ .flox/pkgs/reinvent-demo-ticket-processor.nix
@@ -4,7 +4,7 @@
   python313Packages,
   stdenv,
 }: let
-  version = "0.1.0";
+  version = "0.1.1";
 
   # Python dependencies for the ticket processor
   pythonEnv = python313.withPackages (ps: with ps; [
